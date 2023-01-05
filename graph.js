f9a9588932fab4a30ab26d333a9ab589d28446fb
shohedul350 <shohedul350@gmail.com>
2023-01-05 15:00:58 +0600

    graph and bfs done

--- graph.js
+++ graph.js
@@ -35,35 +35,3 @@ addEdge(2, 4);
 addEdge(3, 0);
 addEdge(3, 4);
 
-
-let visited = new Array(5);
-console.log(visited);
-const bfs = (node)=>{
-  const queue = [];
-
-  for(let i =0; i<visited.length; i++){
-    visited[i] = false;
-  }
-
-visited[node] = true;
-queue.push(node);
-
-while (queue.length) {
-  let currentNode = queue.shift();
-  console.log(`visiting ${currentNode}`);
-
-  for(let j = 0; j< graph[currentNode].length; j++){
-
-    if(graph[currentNode][j] === 1 && visited[j] === false){
-      visited[j] = true;
-      queue.push(j)
-    }
-  }
-
-}
-
-
-}
-
-bfs(0);
-console.log(visited);
